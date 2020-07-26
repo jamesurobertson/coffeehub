baf30d620805d8fe6be646c14fc66c64336552f6
James Robertson <jamesurobertson@gmail.com>
2020-07-26 01:57:22 -0500

    updated activity log

--- src/components/Profile/ActivityLog.js
+++ src/components/Profile/ActivityLog.js
@@ -69,11 +69,11 @@ const Months = [
   "Dec",
 ];
 const ActivityLog = ({ roasts, activity }) => {
-    const [roastList, setRoastList] = useState(null);
-    const [viewDay, setViewDay] = useState(false)
-    const now = new Date();
-    const month = Months[now.getMonth()];
-    const year = now.getFullYear();
+  const [roastList, setRoastList] = useState(null);
+  const [viewDay, setViewDay] = useState(false);
+  const now = new Date();
+  const month = Months[now.getMonth()];
+  const year = now.getFullYear();
 
   useEffect(() => {
     if (activity) {
@@ -84,7 +84,7 @@ const ActivityLog = ({ roasts, activity }) => {
       });
 
       setRoastList(filteredRoasts.sort(sortList));
-      setViewDay(true)
+      setViewDay(true);
     } else {
       setRoastList(roasts.sort(sortList));
     }
@@ -94,8 +94,7 @@ const ActivityLog = ({ roasts, activity }) => {
     return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
   };
 
-
-  const getDate = (timeString) => {
+  const getMonthDay = (timeString) => {
     const date = new Date(timeString);
     const month = Months[date.getMonth()];
     const day = date.getDate();
@@ -103,19 +102,28 @@ const ActivityLog = ({ roasts, activity }) => {
     return `${month} ${day}`;
   };
 
+  const getMonthYear = (timeString ) => {
+    const date = new Date(timeString);
+    const month = Months[date.getMonth()];
+    const year = date.getFullYear();
+
+    return `${month} ${year}`;
+  }
+
   if (!roastList) return null;
   return (
     <ActivityLogWrapper>
       <div style={{ fontSize: "18px" }}> Roast Activity</div>
       <div className="activity-header">
         <h1>
-          {month} {year}
+          {activity ? getMonthYear(activity) : `${month} ${year}` }
         </h1>
         <div className="header-bar" />
       </div>
       <div className="activity-main">
         <div className="activity-main__header">
-          Roasted {roastList.length} {roastList.length === 1 ? 'time' : 'times'} {viewDay ? `on ${getDate(activity)}` : `in the last month`}
+          Roasted {roastList.length} {roastList.length === 1 ? "time" : "times"}{" "}
+          {viewDay ? `on ${getMonthDay(activity)}` : `in the last month`}
         </div>
         <ul>
           {roastList.map((roast) => {
@@ -124,7 +132,7 @@ const ActivityLog = ({ roasts, activity }) => {
                 <Link to={`/r/${roast.roastUser.username}/${roast.name}`}>
                   {`${roast.roastUser.username}/${roast.name}`}
                 </Link>
-                <div className="roast-date">{getDate(roast.createdAt)}</div>
+                <div className="roast-date">{getMonthDay(roast.createdAt)}</div>
               </li>
             );
           })}
